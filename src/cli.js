ee66f90559c5ef0f3ebd7878109fb253f6a2a8fb
mrfurm4n <mr.furmand@gmail.com>
2022-08-02 14:46:22 +0200

    install eslint and fixe eslint errors

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const startGreeting = () => {
-	console.log('Welcome to the Brain Games!');
+export default () => {
+  console.log('Welcome to the Brain Games!');
 
-	const name = readlineSync.question('May I have your name?: ');
-	console.log(`Hello, ${name}!`);
-}
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+};
